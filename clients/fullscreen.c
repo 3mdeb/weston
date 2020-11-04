007c069dfd98b67a833b61ed57b4733fafa8218c
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2020-11-04 19:16:29 +0100

    clients/fullscreen.c: modify to display selected image
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- clients/fullscreen.c
+++ clients/fullscreen.c
@@ -30,12 +30,14 @@
 #include <stdarg.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 #include <errno.h>
 #include <cairo.h>
 
 #include <linux/input.h>
 #include <wayland-client.h>
 #include "window.h"
+#include "shared/cairo-util.h"
 #include "fullscreen-shell-unstable-v1-client-protocol.h"
 #include <libweston/zalloc.h>
 
@@ -50,67 +52,58 @@ struct fullscreen {
 	struct widget *widget;
 	struct zwp_fullscreen_shell_v1 *fshell;
 	enum zwp_fullscreen_shell_v1_present_method present_method;
-	int width, height;
-	int fullscreen;
+	int32_t width, height;
 	float pointer_x, pointer_y;
 	int draw_cursor;
+
+	char *filename;
+	cairo_surface_t *image;
+	bool initialized;
+	cairo_matrix_t matrix;
 
 	struct wl_list output_list;
 	struct fs_output *current_output;
 };
 
-static void
-fullscreen_handler(struct window *window, void *data)
+static double
+get_scale(struct fullscreen *fullscreen)
 {
-	struct fullscreen *fullscreen = data;
-
-	fullscreen->fullscreen ^= 1;
-	window_set_fullscreen(window, fullscreen->fullscreen);
+	assert(fullscreen->matrix.xy == 0.0 &&
+	       fullscreen->matrix.yx == 0.0 &&
+	       fullscreen->matrix.xx == fullscreen->matrix.yy);
+	return fullscreen->matrix.xx;
 }
 
 static void
-draw_string(cairo_t *cr,
-	    const char *fmt, ...)
+clamp_view(struct fullscreen *fullscreen)
 {
-	char buffer[4096];
-	char *p, *end;
-	va_list argp;
-	cairo_text_extents_t text_extents;
-	cairo_font_extents_t font_extents;
+	struct rectangle allocation;
+	double scale = get_scale(fullscreen);
+	double sw, sh;
 
-	cairo_save(cr);
+	sw = fullscreen->width * scale;
+	sh = fullscreen->height * scale;
+	widget_get_allocation(fullscreen->widget, &allocation);
 
-	cairo_select_font_face(cr, "sans",
-			       CAIRO_FONT_SLANT_NORMAL,
-			       CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_font_size(cr, 14);
-
-	cairo_font_extents (cr, &font_extents);
-
-	va_start(argp, fmt);
-
-	vsnprintf(buffer, sizeof(buffer), fmt, argp);
-
-	p = buffer;
-	while (*p) {
-		end = strchr(p, '\n');
-		if (end)
-			*end = 0;
-
-		cairo_show_text(cr, p);
-		cairo_text_extents (cr, p, &text_extents);
-		cairo_rel_move_to (cr, -text_extents.x_advance, font_extents.height);
-
-		if (end)
-			p = end + 1;
-		else
-			break;
+	if (sw < allocation.width) {
+		fullscreen->matrix.x0 =
+			(allocation.width - fullscreen->width * scale) / 2;
+	} else {
+		if (fullscreen->matrix.x0 > 0.0)
+			fullscreen->matrix.x0 = 0.0;
+		if (sw + fullscreen->matrix.x0 < allocation.width)
+			fullscreen->matrix.x0 = allocation.width - sw;
 	}
 
-	va_end(argp);
-
-	cairo_restore(cr);
-
+	if (sh < allocation.height) {
+		fullscreen->matrix.y0 =
+			(allocation.height - fullscreen->height * scale) / 2;
+	} else {
+		if (fullscreen->matrix.y0 > 0.0)
+			fullscreen->matrix.y0 = 0.0;
+		if (sh + fullscreen->matrix.y0 < allocation.height)
+			fullscreen->matrix.y0 = allocation.height - sh;
+	}
 }
 
 static void
@@ -120,9 +113,12 @@ redraw_handler(struct widget *widget, void *data)
 	struct rectangle allocation;
 	cairo_surface_t *surface;
 	cairo_t *cr;
-	int i;
-	double x, y, border;
-	const char *method_name[] = { "default", "center", "zoom", "zoom_crop", "stretch"};
+	double width, height, doc_aspect, window_aspect, scale;
+	cairo_matrix_t matrix;
+	cairo_matrix_t translate;
+	// int i;
+	//double x, y, border;
+	//const char *method_name[] = { "default", "center", "zoom", "zoom_crop", "stretch"};
 
 	surface = window_get_surface(fullscreen->window);
 	if (surface == NULL ||
@@ -135,249 +131,49 @@ redraw_handler(struct widget *widget, void *data)
 
 	cr = widget_cairo_create(widget);
 
-	cairo_set_source_rgb(cr, 0, 0, 0);
-	cairo_paint (cr);
+	cairo_rectangle(cr, allocation.x, allocation.y,
+			allocation.width, allocation.height);
+	cairo_clip(cr);
+	cairo_push_group(cr);
+	cairo_translate(cr, allocation.x, allocation.y);
 
-	cairo_set_source_rgb(cr, 0, 0, 1);
-	cairo_set_line_width (cr, 10);
-	cairo_rectangle(cr, 5, 5, allocation.width - 10, allocation.height - 10);
-	cairo_stroke (cr);
+	cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+	cairo_set_source_rgba(cr, 0, 0, 0, 1);
+	cairo_paint(cr);
 
-	cairo_move_to(cr,
-		      allocation.x + 15,
-		      allocation.y + 25);
-	cairo_set_source_rgb(cr, 1, 1, 1);
+	if (!fullscreen->initialized) {
+		fullscreen->initialized = true;
+		width = cairo_image_surface_get_width(fullscreen->image);
+		height = cairo_image_surface_get_height(fullscreen->image);
 
-	if (fullscreen->fshell) {
-		draw_string(cr,
-			    "Surface size: %d, %d\n"
-			    "Scale: %d, transform: %d\n"
-			    "Pointer: %f,%f\n"
-			    "Output: %s, present method: %s\n"
-			    "Keys: (s)cale, (t)ransform, si(z)e, (m)ethod,\n"
-			    "      (o)utput, modes(w)itch, (q)uit\n",
-			    fullscreen->width, fullscreen->height,
-			    window_get_buffer_scale (fullscreen->window),
-			    window_get_buffer_transform (fullscreen->window),
-			    fullscreen->pointer_x, fullscreen->pointer_y,
-			    method_name[fullscreen->present_method],
-			    fullscreen->current_output ? output_get_model(fullscreen->current_output->output): "null");
-	} else {
-		draw_string(cr,
-			    "Surface size: %d, %d\n"
-			    "Scale: %d, transform: %d\n"
-			    "Pointer: %f,%f\n"
-			    "Fullscreen: %d\n"
-			    "Keys: (s)cale, (t)ransform, si(z)e, (f)ullscreen, (q)uit\n",
-			    fullscreen->width, fullscreen->height,
-			    window_get_buffer_scale (fullscreen->window),
-			    window_get_buffer_transform (fullscreen->window),
-			    fullscreen->pointer_x, fullscreen->pointer_y,
-			    fullscreen->fullscreen);
-	}
-
-	y = 100;
-	i = 0;
-	while (y + 60 < fullscreen->height) {
-		border = (i++ % 2 == 0) ? 1 : 0.5;
-
-		x = 50;
-		cairo_set_line_width (cr, border);
-		while (x + 70 < fullscreen->width) {
-			if (window_has_focus(fullscreen->window) &&
-			    fullscreen->pointer_x >= x && fullscreen->pointer_x < x + 50 &&
-			    fullscreen->pointer_y >= y && fullscreen->pointer_y < y + 40) {
-				cairo_set_source_rgb(cr, 1, 0, 0);
-				cairo_rectangle(cr,
-						x, y,
-						50, 40);
-				cairo_fill(cr);
-			}
-			cairo_set_source_rgb(cr, 0, 1, 0);
-			cairo_rectangle(cr,
-					x + border/2.0, y + border/2.0,
-					50, 40);
-			cairo_stroke(cr);
-			x += 60;
-		}
-
-		y += 50;
-	}
-
-	if (window_has_focus(fullscreen->window) && fullscreen->draw_cursor) {
-		cairo_set_source_rgb(cr, 1, 1, 1);
-		cairo_set_line_width (cr, 8);
-		cairo_move_to(cr,
-			      fullscreen->pointer_x - 12,
-			      fullscreen->pointer_y - 12);
-		cairo_line_to(cr,
-			      fullscreen->pointer_x + 12,
-			      fullscreen->pointer_y + 12);
-		cairo_stroke(cr);
-
-		cairo_move_to(cr,
-			      fullscreen->pointer_x + 12,
-			      fullscreen->pointer_y - 12);
-		cairo_line_to(cr,
-			      fullscreen->pointer_x - 12,
-			      fullscreen->pointer_y + 12);
-		cairo_stroke(cr);
-
-		cairo_set_source_rgb(cr, 0, 0, 0);
-		cairo_set_line_width (cr, 4);
-		cairo_move_to(cr,
-			      fullscreen->pointer_x - 10,
-			      fullscreen->pointer_y - 10);
-		cairo_line_to(cr,
-			      fullscreen->pointer_x + 10,
-			      fullscreen->pointer_y + 10);
-		cairo_stroke(cr);
-
-		cairo_move_to(cr,
-			      fullscreen->pointer_x + 10,
-			      fullscreen->pointer_y - 10);
-		cairo_line_to(cr,
-			      fullscreen->pointer_x - 10,
-			      fullscreen->pointer_y + 10);
-		cairo_stroke(cr);
-	}
-
-	cairo_destroy(cr);
-}
-
-static void
-key_handler(struct window *window, struct input *input, uint32_t time,
-	    uint32_t key, uint32_t sym, enum wl_keyboard_key_state state,
-	    void *data)
-{
-	struct fullscreen *fullscreen = data;
-	int transform, scale;
-	static int current_size = 0;
-	struct fs_output *fsout;
-	struct wl_output *wl_output;
-	int widths[] = { 640, 320, 800, 400 };
-	int heights[] = { 480, 240, 600, 300 };
-
-	if (state == WL_KEYBOARD_KEY_STATE_RELEASED)
-		return;
-
-	switch (sym) {
-	case XKB_KEY_t:
-		transform = window_get_buffer_transform (window);
-		transform = (transform + 1) % 8;
-		window_set_buffer_transform(window, transform);
-		window_schedule_redraw(window);
-		break;
-
-	case XKB_KEY_s:
-		scale = window_get_buffer_scale (window);
-		if (scale == 1)
-			scale = 2;
+		doc_aspect = width / height;
+		window_aspect = (double) allocation.width / allocation.height;
+		if (doc_aspect < window_aspect)
+			scale = allocation.height / height;
 		else
-			scale = 1;
-		window_set_buffer_scale(window, scale);
-		window_schedule_redraw(window);
-		break;
+			scale = allocation.width / width;
 
-	case XKB_KEY_z:
-		if (fullscreen->fullscreen)
-			break;
+		fullscreen->width = width;
+		fullscreen->height = height;
+		cairo_matrix_init_scale(&fullscreen->matrix, scale, scale);
 
-		current_size = (current_size + 1) % 4;
-		fullscreen->width = widths[current_size];
-		fullscreen->height = heights[current_size];
-		window_schedule_resize(fullscreen->window,
-				       fullscreen->width, fullscreen->height);
-		break;
-
-	case XKB_KEY_m:
-		if (!fullscreen->fshell)
-			break;
-
-		wl_output = NULL;
-		if (fullscreen->current_output)
-			wl_output = output_get_wl_output(fullscreen->current_output->output);
-		fullscreen->present_method = (fullscreen->present_method + 1) % 5;
-		zwp_fullscreen_shell_v1_present_surface(fullscreen->fshell,
-							window_get_wl_surface(fullscreen->window),
-							fullscreen->present_method,
-							wl_output);
-		window_schedule_redraw(window);
-		break;
-
-	case XKB_KEY_o:
-		if (!fullscreen->fshell)
-			break;
-
-		fsout = fullscreen->current_output;
-		wl_output = fsout ? output_get_wl_output(fsout->output) : NULL;
-
-		/* Clear the current presentation */
-		zwp_fullscreen_shell_v1_present_surface(fullscreen->fshell, NULL,
-							0, wl_output);
-
-		if (fullscreen->current_output) {
-			if (fullscreen->current_output->link.next == &fullscreen->output_list)
-				fsout = NULL;
-			else
-				fsout = wl_container_of(fullscreen->current_output->link.next,
-							fsout, link);
-		} else {
-			fsout = wl_container_of(fullscreen->output_list.next,
-						fsout, link);
-		}
-
-		fullscreen->current_output = fsout;
-		wl_output = fsout ? output_get_wl_output(fsout->output) : NULL;
-		zwp_fullscreen_shell_v1_present_surface(fullscreen->fshell,
-							window_get_wl_surface(fullscreen->window),
-							fullscreen->present_method,
-							wl_output);
-		window_schedule_redraw(window);
-		break;
-
-	case XKB_KEY_w:
-		if (!fullscreen->fshell || !fullscreen->current_output)
-			break;
-
-		wl_output = NULL;
-		if (fullscreen->current_output)
-			wl_output = output_get_wl_output(fullscreen->current_output->output);
-		zwp_fullscreen_shell_mode_feedback_v1_destroy(
-			zwp_fullscreen_shell_v1_present_surface_for_mode(fullscreen->fshell,
-									 window_get_wl_surface(fullscreen->window),
-									 wl_output, 0));
-		window_schedule_redraw(window);
-		break;
-
-	case XKB_KEY_f:
-		if (fullscreen->fshell)
-			break;
-		fullscreen->fullscreen ^= 1;
-		window_set_fullscreen(window, fullscreen->fullscreen);
-		break;
-
-	case XKB_KEY_q:
-		exit (0);
-		break;
+		clamp_view(fullscreen);
 	}
-}
 
-static int
-motion_handler(struct widget *widget,
-	       struct input *input,
-	       uint32_t time,
-	       float x,
-	       float y, void *data)
-{
-	struct fullscreen *fullscreen = data;
+	matrix = fullscreen->matrix;
+	cairo_matrix_init_translate(&translate, allocation.x, allocation.y);
+	cairo_matrix_multiply(&matrix, &matrix, &translate);
+	cairo_set_matrix(cr, &matrix);
 
-	fullscreen->pointer_x = x;
-	fullscreen->pointer_y = y;
+	cairo_set_source_surface(cr, fullscreen->image, 0, 0);
+	cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
+	cairo_paint(cr);
 
-	widget_schedule_redraw(widget);
+	cairo_pop_group_to_source(cr);
+	cairo_paint(cr);
+	cairo_destroy(cr);
 
-	return fullscreen->draw_cursor ? CURSOR_BLANK : CURSOR_LEFT_PTR;
+	cairo_surface_destroy(surface);
 }
 
 static int
@@ -393,35 +189,6 @@ enter_handler(struct widget *widget,
 	widget_schedule_redraw(widget);
 
 	return fullscreen->draw_cursor ? CURSOR_BLANK : CURSOR_LEFT_PTR;
-}
-
-static void
-button_handler(struct widget *widget,
-	       struct input *input, uint32_t time,
-	       uint32_t button, enum wl_pointer_button_state state, void *data)
-{
-	struct fullscreen *fullscreen = data;
-
-	switch (button) {
-	case BTN_LEFT:
-		if (state == WL_POINTER_BUTTON_STATE_PRESSED)
-			window_move(fullscreen->window, input,
-				    display_get_serial(fullscreen->display));
-		break;
-	case BTN_RIGHT:
-		if (state == WL_POINTER_BUTTON_STATE_PRESSED)
-			window_show_frame_menu(fullscreen->window, input, time);
-		break;
-	}
-}
-
-static void
-touch_handler(struct widget *widget, struct input *input,
-		   uint32_t serial, uint32_t time, int32_t id,
-		   float x, float y, void *data)
-{
-	struct fullscreen *fullscreen = data;
-	window_move(fullscreen->window, input, display_get_serial(fullscreen->display));
 }
 
 static void
@@ -496,14 +263,20 @@ int main(int argc, char *argv[])
 	struct display *d;
 	int i;
 
-	fullscreen.width = 640;
-	fullscreen.height = 480;
-	fullscreen.fullscreen = 0;
+	if (argc <= 1 || argv[1][0]=='-') {
+		printf("Usage: %s image...\n", argv[0]); return 1;
+	}
+
+	fullscreen.filename = strdup(argv[1]);
+	printf("fullscreen.filename: %s", fullscreen.filename);
+
+	fullscreen.width = 200;
+	fullscreen.height = 100;
 	fullscreen.present_method = ZWP_FULLSCREEN_SHELL_V1_PRESENT_METHOD_DEFAULT;
 	wl_list_init(&fullscreen.output_list);
 	fullscreen.current_output = NULL;
 
-	for (i = 1; i < argc; i++) {
+	for (i = 2; i < argc; i++) {
 		if (strcmp(argv[i], "-w") == 0) {
 			if (++i >= argc)
 				usage(EXIT_FAILURE);
@@ -529,6 +302,14 @@ int main(int argc, char *argv[])
 
 	fullscreen.display = d;
 	fullscreen.fshell = NULL;
+
+	fullscreen.image = load_cairo_surface(argv[1]);
+
+	if (!fullscreen.image) {
+		free(fullscreen.filename);
+		return -1;
+	}
+
 	display_set_user_data(fullscreen.display, &fullscreen);
 	display_set_global_handler(fullscreen.display, global_handler);
 	display_set_output_configure_handler(fullscreen.display, output_handler);
@@ -555,14 +336,10 @@ int main(int argc, char *argv[])
 
 	widget_set_default_cursor(fullscreen.widget, CURSOR_LEFT_PTR);
 	widget_set_redraw_handler(fullscreen.widget, redraw_handler);
-	widget_set_button_handler(fullscreen.widget, button_handler);
-	widget_set_motion_handler(fullscreen.widget, motion_handler);
 	widget_set_enter_handler(fullscreen.widget, enter_handler);
 
-	widget_set_touch_down_handler(fullscreen.widget, touch_handler);
-
-	window_set_key_handler(fullscreen.window, key_handler);
-	window_set_fullscreen_handler(fullscreen.window, fullscreen_handler);
+	/* force fullscreen mode */
+	window_set_fullscreen(fullscreen.window, 1);
 
 	window_set_user_data(fullscreen.window, &fullscreen);
 	/* Hack to set minimum allocation so we can shrink later */
